3ea0ea7fcb98f67e9d6cb508ea3cb7dbfac3ddfb
hanna-skryl <hanna.skryl@gmail.com>
2024-09-01 16:35:03 -0400

    refactor: extract to-do item into component

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,24 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
+import { TodoStore } from '../store/todo.store';
+import { Item } from '../models';
+import { CdkDrag } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [CdkDrag],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TodoItemComponent {
+  readonly store = inject(TodoStore);
+
+  item = input.required<Item>();
+  id = input.required<string>();
+}

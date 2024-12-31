8c566151c1674ef088a95f0e7deb439165ce6918
hanna-skryl <hanna.skryl@gmail.com>
2024-12-31 15:02:54 -0500

    feat: configure routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,14 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {
-    path: '',
+    path: 'dashboard',
     loadComponent: () =>
       import('./todo-list/todo-list.component').then(c => c.TodoListComponent),
   },
   {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full',
+    path: 'presets',
+    loadComponent: () =>
+      import('./presets/presets.component').then(c => c.PresetsComponent),
   },
+  {
+    path: 'presets/:id',
+    loadComponent: () =>
+      import('./preset/preset.component').then(c => c.PresetComponent),
+  },
+  { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
---
+++ client/src/app/preset/preset.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preset',
+  imports: [],
+  templateUrl: './preset.component.html',
+  styleUrl: './preset.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PresetComponent {
+
+}

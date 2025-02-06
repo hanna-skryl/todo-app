25fa2b1bfcc83d7d23ff9473511c382100cb2d60
hanna-skryl <hanna.skryl@gmail.com>
2025-02-05 21:54:19 -0500

    feat: add preset editing functionality

--- client/src/app/todo-item/todo-item.component.ts
+++ client/src/app/todo-item/todo-item.component.ts
@@ -1,23 +1,83 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
+  forwardRef,
   inject,
   input,
+  OnInit,
+  output,
 } from '@angular/core';
 import { TodoStore } from '../store/todo.store';
 import { Item } from '../models';
 import { CdkDrag } from '@angular/cdk/drag-drop';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common';
+
+export type TodoItemEvent = {
+  item: Item;
+  event: 'delete' | 'update';
+};
 
 @Component({
   selector: 'app-todo-item',
-  imports: [CdkDrag],
+  imports: [CdkDrag, NgOptimizedImage, ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TodoItemComponent),
+      multi: true,
+    },
+  ],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TodoItemComponent {
+export class TodoItemComponent implements ControlValueAccessor {
   readonly store = inject(TodoStore);
 
-  item = input.required<Item>();
-  id = input.required<string>();
+  readonly todo = output<TodoItemEvent>();
+
+  readonly item = input.required<Item>();
+  readonly id = input.required<string>();
+
+  readonly todoControl = new FormControl<boolean>(false, { nonNullable: true });
+
+  private onChange: ((value: boolean) => void) | undefined;
+  private onTouched: (() => void) | undefined;
+
+  get isInteractive(): boolean {
+    return this.onChange !== undefined;
+  }
+
+  writeValue(value: boolean | null): void {
+    if (value !== null) {
+      this.todoControl.setValue(value, { emitEvent: false });
+    }
+  }
+  registerOnChange(fn: (value: boolean) => void): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  deleteTodo(): void {
+    this.todo.emit({
+      item: { ...this.item(), done: this.todoControl.value },
+      event: 'delete',
+    });
+  }
+
+  toggleDone(): void {
+    this.todo.emit({
+      item: { ...this.item(), done: this.todoControl.value },
+      event: 'update',
+    });
+  }
 }

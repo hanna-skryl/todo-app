a8f3c75ca5315c4f536f6df4a747321c833f0728
hanna-skryl <hanna.skryl@gmail.com>
2025-03-09 15:23:51 -0400

    refactor(auth): simplify authGuard logic and improve readability

--- client/src/app/landing/auth.guard.ts
+++ client/src/app/landing/auth.guard.ts
@@ -2,13 +2,15 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
-export const authGuard: CanActivateFn = async () => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
+export const authGuard = (): CanActivateFn => {
+  return () => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
 
-  return authService.isInitialized()
-    ? authService.isLoggedIn()
-      ? true
-      : router.createUrlTree(['/login'])
-    : false;
+    if (authService.isInitialized() && authService.isLoggedIn()) {
+      return true;
+    }
+
+    return router.parseUrl('/login');
+  };
 };

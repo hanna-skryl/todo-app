1afd148e816daee65a3d73fa7926e703e7549db6
hanna-skryl <hanna.skryl@gmail.com>
2025-12-23 16:08:55 -0500

    refactor(preset): use route input binding and improve reactivity

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,7 +2,7 @@ import {
   ApplicationConfig,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routing';
 import { provideStore } from '@ngrx/store';
@@ -11,7 +11,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideStore(),
     provideHttpClient(withFetch()),
   ],

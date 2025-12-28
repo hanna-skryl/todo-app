eb566af47929a5095a3074107f4845fe22e0a604
hanna-skryl <hanna.skryl@gmail.com>
2025-12-28 12:30:36 -0500

    fix(toast): correct variable name for message subject

--- client/src/app/shared/toast/toast.service.ts
+++ client/src/app/shared/toast/toast.service.ts
@@ -3,10 +3,10 @@ import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ToastService {
-  private readonly messageSubject$$ = new Subject<string>();
-  readonly message$ = this.messageSubject$$.asObservable();
+  private readonly messageSubject$ = new Subject<string>();
+  readonly message$ = this.messageSubject$.asObservable();
 
   showMessage(message: string): void {
-    this.messageSubject$$.next(message);
+    this.messageSubject$.next(message);
   }
 }

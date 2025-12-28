ace0a562a5d5e9a57d169add29e615713ba89b4e
hanna-skryl <hanna.skryl@gmail.com>
2025-12-28 12:57:55 -0500

    feat(toast): add error handling and display for toast notifications

--- client/src/app/shared/toast/toast.service.ts
+++ client/src/app/shared/toast/toast.service.ts
@@ -6,7 +6,14 @@ export class ToastService {
   private readonly messageSubject$ = new Subject<string>();
   readonly message$ = this.messageSubject$.asObservable();
 
+  private readonly errorSubject$ = new Subject<string>();
+  readonly error$ = this.errorSubject$.asObservable();
+
   showMessage(message: string): void {
     this.messageSubject$.next(message);
+  }
+
+  showError(message: string): void {
+    this.errorSubject$.next(message);
   }
 }
